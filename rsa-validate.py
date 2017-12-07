ba46c1b633c6956aa835f41cf950907bb39ffb85
dsande30 <dsande30@vols.utk.edu>
2017-12-06 21:17:29 -0500

    Added random number for sig, finished validate(?)

--- rsa-validate.py
+++ rsa-validate.py
@@ -1,42 +1,71 @@
 import argparse
+from Crypto.Hash import SHA256
 
 def getFlags():
     #parse command line args
     parser = argparse.ArgumentParser()
     parser.add_argument("-k", dest = 'keyFile', help="Enter key file", required = True)
     parser.add_argument("-m", dest = 'msgFile', help="Enter message file", required = True)
-    parser.add_argument("-o", dest = 'sigFile', help= "Enter signature file", required=True)
+    parser.add_argument("-s", dest = 'sigFile', help= "Enter signature file", required=True)
     args = parser.parse_args()
     return args
 
 def openFiles(args):
     #keyFile
-    print("Key file: %s" % args.keyFile)
     fd = open(args.keyFile, "r")
-    for line in fd:
-        print("%s" % line.strip())
-    print("\n")
+    numBits = fd.readline()
+    numBits = int(numBits.strip())
+    N = fd.readline()
+    N = int(N.strip())
+    pubKey = fd.readline()
+    pubKey = int(pubKey.strip())
+    keyTuple = (numBits, N, pubKey)
     fd.close()
 
     #msgFile
-    print("Message File: %s" % args.msgFile)
-    fd = open(args.msgFile, "r")
-    for line in fd:
-        print("%s" % line.strip())
-    print("\n")
+    msg = ""
+    fd = open(args.msgFile, "rb")
+    while True:
+        string = fd.readline()
+        if string == "":
+                break
+        msg += string
     fd.close()
 
     #sigFile
-    print("Signature File: %s" % args.sigFile)
     fd = open(args.sigFile, "r")
-    for line in fd:
-        print("%s" % line.strip())
-    print("\n")
+    sig = ""
+    while True:
+        string = fd.readline()
+        if string == "":
+            break
+        sig += string
+    sig = int(sig)
     fd.close()
+
+    return (keyTuple, msg, sig)
+
+def hash(message):
+    h = SHA256.new()
+    h.update(message)
+    return h
+
+def verify(h, sig, N, e):
+    result = pow(sig, e)
+    if result == (int(h.hexdigest(), 16) % N):
+        return 1
+    else:
+        return 0
 
 def main():
     args = getFlags()
-    openFiles(args)
+    results = openFiles(args)
+    h = hash(results[1])
+    value = verify(h, results[2], results[0][1], results[0][2])
+    if value == 1:
+        print("True")
+    else:
+        print("False")
 
 if __name__ == "__main__":
     main()
